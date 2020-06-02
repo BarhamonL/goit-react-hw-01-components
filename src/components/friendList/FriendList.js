a802f91e5310923a5f6028ad7a40b4413c02945b
lenya1976 <bargamon76@gmail.comgit config --global user.name Your>
2020-06-02 16:26:14 +0300

    homework01

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FriendList.module.css';
+import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = function ({ friends }) {
   return (
     <ul className={styles.friendList}>
-      {friends.map(friend => (
-        <li key={friend.id} className={styles.item}>
-          <span
-            className={friend.isOnline ? styles.onLine : styles.offLine}
-          ></span>
-          <img
-            className={styles.avatar}
-            src={friend.avatar}
-            alt={friend.name}
-            width="48"
-          />
-          <p className={styles.name}>{friend.name}</p>
-        </li>
-      ))}
+      {friends.map(friend => {
+        return <FriendListItem key={friend.id} friend={friend} />;
+      })}
     </ul>
   );
 };
