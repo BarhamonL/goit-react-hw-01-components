931801c600c5376d231289ecd613415b7116cfde
lenya1976 <bargamon76@gmail.comgit config --global user.name Your>
2020-06-02 13:46:31 +0300

    Updates

---
+++ precache-manifest.b7cdd5340cc417dd82481c27ee180767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5166aaeab9c7b4c4141a3cfe67c668b9",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "4cbdc214e330ce9875f1",
+    "url": "/goit-react-hw-01-components/static/css/main.10b95534.chunk.css"
+  },
+  {
+    "revision": "99919af9b0d94aaa0bb8",
+    "url": "/goit-react-hw-01-components/static/js/2.befd264b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.befd264b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbdc214e330ce9875f1",
+    "url": "/goit-react-hw-01-components/static/js/main.6d23df3a.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
